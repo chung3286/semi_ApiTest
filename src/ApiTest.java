1df8124690bda162cce2159ccd583766c82bb7c3
GyeongEun <chung3286@konkuk.ac.kr>
2020-05-09 00:04:27 +0900

    "Print the codes of nations"

--- src/ApiTest.java
+++ src/ApiTest.java
@@ -7,10 +7,12 @@ import org.json.simple.parser.JSONParser;
  
 public class ApiTest {
 	public static int num = 1;
+	public static int nationNm = 0;
     public ApiTest() throws Exception{
          
          JSONParser jsonparser = new JSONParser();
          JSONObject jsonobject = (JSONObject)jsonparser.parse(readUrl());
+         /*
          JSONObject json =  (JSONObject) jsonobject.get("movieListResult");
          JSONArray array = (JSONArray)json.get("movieList");
          for(int i = 0 ; i < array.size(); i++) {
@@ -27,12 +29,30 @@ public class ApiTest {
             	 System.out.println("Page " + num + ", " + (i+1) + " " + movieNm);           
          }     
          num++;
+         */
+          JSONArray array = (JSONArray) jsonobject.get("codes");
+         //System.out.println(array);
+         for(int i = 0 ; i < array.size(); i++) {
+             JSONObject entity = (JSONObject)array.get(i);
+             //System.out.println(entity);
+             String korNm = (String) entity.get("korNm");
+             String fullCd = (String) entity.get("fullCd");
+             System.out.printf("%d %s  code : %-10s  / ", i+1, korNm, fullCd);
+             if(i%5==4)
+            	 System.out.println();
+         }     
+         num++;
+         
     }
     private static String readUrl() throws Exception {
         BufferedInputStream reader = null;
         try {
-            URL url = new URL("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=ed31013ae071bc9449eec7b79b961036&itemPerPage=5&curPage=" + num);
-            reader = new BufferedInputStream(url.openStream());
+        	// 영화 추천 목록
+            //URL url = new URL("http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=ed31013ae071bc9449eec7b79b961036&itemPerPage=5&curPage=" + num + "&repNationCd=" + nationNm);
+        	// 나라 고르기 코드 출력
+            URL url = new URL("http://www.kobis.or.kr/kobisopenapi/webservice/rest/code/searchCodeList.json?key=ed31013ae071bc9449eec7b79b961036&comCode="+2204);
+        	
+        	reader = new BufferedInputStream(url.openStream());
             StringBuffer buffer = new StringBuffer();
             int i;
             byte[] b = new byte[4096];
@@ -51,7 +71,8 @@ public class ApiTest {
     	Scanner sc = new Scanner(System.in);
         // TODO Auto-generated method stub
         try {
-        	for(int i=0; i<3; i++) {
+        	nationNm = 22042002;
+        	for(int i=0; i<4; i++) {
                 new ApiTest();
                 
         	}
@@ -64,3 +85,6 @@ public class ApiTest {
     }
  
 }
+
+
+// 위키피디아 기본 https://ko.wikipedia.org/wiki/ + 검색어      
